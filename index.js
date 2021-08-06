9b69b16138887c75fe357dc57067fd047ea19a75
ny-yohei <yo0314baseball@gmail.com>
2021-08-06 14:18:24 +0900

    1-3

---
+++ index.js
@@ -0,0 +1,34 @@
+let test = "hello word";
+
+// test ="hello World23"
+
+// 定数
+const name1 ="hello"
+
+
+let inoki=["1","2","3","4"]
+
+console.log(test);
+
+console.log(name1);
+// console.log(inoki[1]);
+
+let index=0
+// while(index < 5){
+
+//   // 繰り返し処理
+//    index++;
+//    console.log(index)
+
+// }
+
+// for (let index = 0; index < inoki.length; index++) {
+//   const practice = inoki[index];
+//   console.log(practice)
+  
+// }
+
+
+
+
+if 
